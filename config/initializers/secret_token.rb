0b265a1f7cce52883b8e2a4c15d992508805e438
Yangzi Liu <yangzi@levion.com>
2011-06-16 11:43:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test2::Application.config.secret_token = 'f5208e6eda7702b1af9ca864bb5d7c9bcbf99d8ea58e1066e84a04b48f4e9f07458b17453fbd6e187d298ff35ef86730de4fd769ddd3725df60dc54a91f9cb6a'
